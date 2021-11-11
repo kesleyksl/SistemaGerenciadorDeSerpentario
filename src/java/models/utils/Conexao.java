060d29ac839deef96aeecd93abd13419e40292df
Kesley de Mello <32515351+kesleyksl@users.noreply.github.com>
2021-11-11 18:46:16 -0300

    Postgres conection string

--- src/java/models/utils/Conexao.java
+++ src/java/models/utils/Conexao.java
@@ -12,9 +12,9 @@ public class Conexao {
 
     public static Connection conectar() {
         Connection con = null;
-        String url = "jdbc:postgresql://localhost:5432/Serpentario";
-        String user = "postgres";
-        String password = "admin";
+        String url = "postgres://qilvwikuphbqeq:13134288709e017e18a5b46e5a546081d2371ee46c67f2ec3252e749b4d7abb5@ec2-50-19-176-236.compute-1.amazonaws.com:5432/da8sasln08evl6";
+        String user = "qilvwikuphbqeq";
+        String password = "13134288709e017e18a5b46e5a546081d2371ee46c67f2ec3252e749b4d7abb5";
         try {
             Class.forName("org.postgresql.Driver");
             con = DriverManager.getConnection(url, user, password);
